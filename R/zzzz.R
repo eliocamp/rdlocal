819e2680117801f40e15831c2ad583120390cbae
C. Regouby <christophe.regouby@free.fr>
2025-02-15 16:47:42 +0100

    Insert translation within parse_Rd workflow

--- R/zzzz.R
+++ R/zzzz.R
@@ -8,4 +8,24 @@
   unlockBinding('.getHelpFile', utils)
   assign('.getHelpFile', getHelpFile, envir = utils)
   lockBinding('.getHelpFile', utils)
+
+  # pkgdown <- loadNamespace('pkgdown')
+  # rd_text <- get('rd_text', envir = pkgdown)
+  # body(rd_text)[[2]] <- substitute({
+  #   on.exit(return(rhelpi18n:::.translateHelpFile(returnValue(), pkgname, file)))
+  #   step1
+  # }, list(step1 = body(rd_text)[[2]]))
+  # unlockBinding('rd_text', pkgdown)
+  # assign('rd_text', rd_text, envir = pkgdown)
+  # lockBinding('rd_text', pkgdown)
+  #
+  tools <- loadNamespace('tools')
+  parse_Rd <- get('parse_Rd', envir = tools)
+  body(parse_Rd)[[2]] <- substitute({
+    on.exit(return(rhelpi18n:::.translateHelpFile(returnValue(), pkgname, file)))
+    step1
+  }, list(step1 = body(parse_Rd)[[2]]))
+  unlockBinding('parse_Rd', tools)
+  assign('parse_Rd', parse_Rd, envir = tools)
+  lockBinding('parse_Rd', tools)
 }
